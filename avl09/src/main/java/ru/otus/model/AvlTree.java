0ef9eb93649d932589d394fc66c4b19cde98fe9e
Serjes <serjes@yandex.ru>
2019-06-27 18:05:51 +0300

    saving changes

--- avl09/src/main/java/ru/otus/model/AvlTree.java
+++ avl09/src/main/java/ru/otus/model/AvlTree.java
@@ -1,7 +1,28 @@
 package ru.otus.model;
 
-public class AvlTree<Key, Value> implements ITree<Key, Value> {
+public class AvlTree<Key extends Comparable<Key>, Value> implements ITree<Key, Value> {
+    private int size = 0;
+    private TreeLeaf root;
 
+    private class TreeLeaf<Key, Value> {
+        Key key;
+        Value value;
+        int balance;
+        int height;
+        TreeLeaf parent;
+        TreeLeaf left;
+        TreeLeaf right;
+
+        public TreeLeaf(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    @Override
+    public void find(Key k) {
+
+    }
 
     @Override
     public void insert(Key k, Value v) {
@@ -16,5 +37,19 @@ public class AvlTree<Key, Value> implements ITree<Key, Value> {
     @Override
     public void rebalance() {
 
+    }
+
+    private int height(TreeLeaf x, TreeLeaf y){
+        if(x == null && y == null) return 0;
+        else if(x == null) return y.height;
+        else if(y == null) return x.height;
+        else return Math.max(x.height, y.height);
+    }
+
+    private int balance(TreeLeaf x, TreeLeaf y){
+        if(x == null && y == null) return 0;
+        else if(x == null) return - y.height;
+        else if(y == null) return x.height;
+        else return x.height - y.height;
     }
 }
--- avl09/src/main/java/ru/otus/model/ITree.java
+++ avl09/src/main/java/ru/otus/model/ITree.java
@@ -1,6 +1,7 @@
 package ru.otus.model;
 
-public interface ITree<Key, Value> {
+public interface ITree<Key extends Comparable<Key>, Value> {
+    void find(Key k);
     void insert(Key k, Value v);
     void remove(Key k);
     void rebalance();
